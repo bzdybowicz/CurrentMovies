2d401025b839785d1bc6ac5bf24fad3f0b2257c4
bzdybowicz <bartlomiej.zdybowicz@gmail.com>
2023-11-06 23:39:18 +0100

    Add testing placeholders

---
+++ CurrentMovies/CurrentMoviesTests/Movies/Services/ImageServiceTests.swift
@@ -0,0 +1,13 @@
+//
+//  ImageServiceTests.swift
+//  CurrentMoviesTests
+//
+//  Created by Bartlomiej Zdybowicz on 06/11/2023.
+//
+
+@testable import CurrentMovies
+import XCTest
+
+final class ImageServiceTests: XCTestCase {
+    // TODO: Test service.
+}
---
+++ CurrentMovies/CurrentMoviesTests/Movies/UI/DetailViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDetailViewModelTests.swift
+//  CurrentMoviesTests
+//
+//  Created by Bartlomiej Zdybowicz on 06/11/2023.
+//
+
+@testable import CurrentMovies
+import XCTest
+
+final class MovieDetailViewModelTests: XCTestCase {
+    // TODO: Test view model.
+}
